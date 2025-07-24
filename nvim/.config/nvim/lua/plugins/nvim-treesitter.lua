91e6b986d65b9b16d22ed904a1d283724f946d24
nimelo <mr.nimelo@gmail.com>
2025-07-24 22:30:19 +0100

    consistent highlighting

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -5,7 +5,10 @@ return {
 	config = function()
 		require("nvim-treesitter.configs").setup({
 			auto_install = true,
-			highlight = { enable = true },
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
 		})
 	end,
 }
