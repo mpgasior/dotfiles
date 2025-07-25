4b62b0ed9430eab0e26dd334237f44e685591376
nimelo <mr.nimelo@gmail.com>
2025-07-25 15:39:29 +0100

    nvim-tree now follows the current opened buffer

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -5,7 +5,11 @@ return {
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
 	},
-	opts = {},
+	opts = {
+		update_focused_file = {
+			enable = true,
+		},
+	},
 	keys = {
 		--{
 		--	"<leader>e",
