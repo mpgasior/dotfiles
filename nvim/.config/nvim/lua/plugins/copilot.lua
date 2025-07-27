3e52a6bbb206972e410c46bd793bd27c99b65712
nimelo <mr.nimelo@gmail.com>
2025-07-27 21:34:02 +0100

    other things

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -6,9 +6,9 @@ return {
 			{ "nvim-lua/plenary.nvim" },
 		},
 		build = "make tiktoken",
-		opts = {},
 		keys = {
 			{ "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
+			{ "<leader>a?", "<cmd>CopilotChatModels<cr>", desc = "Select Models (CopilotChat)" },
 			{
 				"<leader>aa",
 				function()
@@ -47,6 +47,68 @@ return {
 				desc = "Prompt Actions (CopilotChat)",
 				mode = { "n", "v" },
 			},
+			{
+				"<leader>as",
+				function()
+					require("CopilotChat").stop()
+				end,
+				desc = "Stop Actions (CopilotChat)",
+				mode = { "n", "v" },
+			},
 		},
+		opts = function()
+			return {
+				providers = {
+					github_models = { disabled = true },
+					copilot = { disabled = true },
+					ollama = {
+						prepare_input = require("CopilotChat.config.providers").copilot.prepare_input,
+						prepare_output = require("CopilotChat.config.providers").copilot.prepare_output,
+
+						get_models = function(headers)
+							local response, err =
+								require("CopilotChat.utils").curl_get("http://localhost:11434/v1/models", {
+									headers = headers,
+									json_response = true,
+								})
+
+							if err then
+								error(err)
+							end
+
+							return vim.tbl_map(function(model)
+								return {
+									id = model.id,
+									name = model.id,
+								}
+							end, response.body.data)
+						end,
+
+						embed = function(inputs, headers)
+							local response, err =
+								require("CopilotChat.utils").curl_post("http://localhost:11434/v1/embeddings", {
+									headers = headers,
+									json_request = true,
+									json_response = true,
+									body = {
+										input = inputs,
+										model = "all-minilm",
+									},
+								})
+
+							if err then
+								error(err)
+							end
+
+							return response.body.data
+						end,
+
+						get_url = function()
+							return "http://localhost:11434/v1/chat/completions"
+						end,
+					},
+				},
+			}
+		end,
 	},
 }
