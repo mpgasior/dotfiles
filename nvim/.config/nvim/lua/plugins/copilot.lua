cd53cec6c225be195bd575ef74f69404be0aede4
nimelo <mr.nimelo@gmail.com>
2025-07-27 17:40:03 +0100

    fixed

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -2,9 +2,7 @@ return {
 	{
 		"CopilotC-Nvim/CopilotChat.nvim",
 		dependencies = {
-			{
-				"github/copilot.vim",
-			},
+			{ "github/copilot.vim" },
 			{ "nvim-lua/plenary.nvim" },
 		},
 		build = "make tiktoken",
