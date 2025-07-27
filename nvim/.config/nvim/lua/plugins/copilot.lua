3cd4967d695b4f10b925b56be669d103ca14979a
nimelo <mr.nimelo@gmail.com>
2025-07-27 19:48:15 +0100

    copilot keybinds

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -7,5 +7,46 @@ return {
 		},
 		build = "make tiktoken",
 		opts = {},
+		keys = {
+			{ "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
+			{
+				"<leader>aa",
+				function()
+					return require("CopilotChat").toggle()
+				end,
+				desc = "Toggle (CopilotChat)",
+				mode = { "n", "v" },
+			},
+			{
+				"<leader>ax",
+				function()
+					return require("CopilotChat").reset()
+				end,
+				desc = "Clear (CopilotChat)",
+				mode = { "n", "v" },
+			},
+			{
+				"<leader>aq",
+				function()
+					vim.ui.input({
+						prompt = "Quick Chat: ",
+					}, function(input)
+						if input ~= "" then
+							require("CopilotChat").ask(input)
+						end
+					end)
+				end,
+				desc = "Quick Chat (CopilotChat)",
+				mode = { "n", "v" },
+			},
+			{
+				"<leader>ap",
+				function()
+					require("CopilotChat").select_prompt()
+				end,
+				desc = "Prompt Actions (CopilotChat)",
+				mode = { "n", "v" },
+			},
+		},
 	},
 }
