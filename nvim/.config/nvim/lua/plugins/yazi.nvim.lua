5a801b7ec40b698b023f9409222f9dadff2752d0
nimelo <mr.nimelo@gmail.com>
2025-08-21 18:36:04 +0100

    yazi

---
+++ nvim/.config/nvim/lua/plugins/yazi.nvim.lua
@@ -0,0 +1,42 @@
+---@type LazySpec
+return {
+  "mikavilpas/yazi.nvim",
+  version = "*",
+  event = "VeryLazy",
+  dependencies = {
+    { "nvim-lua/plenary.nvim", lazy = true },
+  },
+  keys = {
+    {
+      "<leader>-",
+      mode = { "n", "v" },
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
+    },
+    {
+      -- Open in the current working directory
+      "<leader>cw",
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open the file manager in nvim's working directory",
+    },
+    {
+      "<c-up>",
+      "<cmd>Yazi toggle<cr>",
+      desc = "Resume the last yazi session",
+    },
+  },
+  ---@type YaziConfig | {}
+  opts = {
+    open_for_directories = false,
+    keymaps = {
+      show_help = "<f1>",
+    },
+    integrations = {
+      grep_in_selected_files = "fzf-lua",
+      grep_in_directory = "fzf-lua",
+    },
+  },
+  init = function()
+    vim.g.loaded_netrwPlugin = 1
+  end,
+}
