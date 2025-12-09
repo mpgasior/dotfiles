fecaea4c072eafd16196d9f6e132f1796d26f7c9
Mateusz Gasior <mr.nimelo@gmail.com>
2025-12-09 17:52:09 +0000

    Removed unused keybindings + simplified fzf-lua bindings

--- nvim/.config/nvim/lua/keybindings.lua
+++ nvim/.config/nvim/lua/keybindings.lua
@@ -1,10 +1,2 @@
 -- Terminal Mode
 vim.keymap.set("t", "<ESC>", "<C-\\><C-N>", { desc = "Escape Terminal Mode" })
-
--- Move in Insert Mode
-vim.keymap.set("i", "<C-h>", "<Left>", { desc = "Move <Left> in Insert Mode" })
-vim.keymap.set("i", "<C-l>", "<Right>", { desc = "Move <Right> in Insert Mode" })
-
--- Save / Update
-vim.api.nvim_set_keymap("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "<C-s>", "<C-o>:w<CR>", { noremap = true, silent = true })
--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -1,29 +1,17 @@
 return {
-	{
-		"ibhagwan/fzf-lua",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		opts = {},
-		init = function()
-			local is_inside_work_tree = {}
-
-			vim.keymap.set("n", "<leader>ff", function()
-				local cwd = vim.fn.getcwd()
-				if is_inside_work_tree[cwd] == nil then
-					vim.fn.system("git rev-parse --is-inside-work-tree")
-					is_inside_work_tree[cwd] = vim.v.shell_error == 0
-				end
-
-				if is_inside_work_tree[cwd] then
-					require("fzf-lua").git_files()
-				else
-					require("fzf-lua").files()
-				end
-			end, { desc = "FzfLua find files (git or all)" })
-			vim.keymap.set("n", "<leader>ft", "<CMD> FzfLua<CR>", { desc = "FzfLua" })
-			vim.keymap.set("n", "<leader>fg", function()
-				require("fzf-lua").live_grep()
-			end, { desc = "FzfLua live grep" })
-			vim.keymap.set("n", "<leader>fb", "<CMD> FzfLua grep_curbuf<CR>", { desc = "FzfLua current buffer ff" })
-		end,
-	},
+  {
+    "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {},
+    init = function()
+      vim.keymap.set("n", "<leader>faf", "<CMD> FzfLua files<CR>", { desc = "FzfLua files" })
+      vim.keymap.set("n", "<leader>fgf", "<CMD> FzfLua git_files<CR>", { desc = "FzfLua git_files" })
+      vim.keymap.set("n", "<leader>fd", "<CMD> FzfLua diagnostics_document<CR>", { desc = "FzfLua diagnostics_document" })
+      vim.keymap.set("n", "<leader>ft", "<CMD> FzfLua<CR>", { desc = "FzfLua" })
+      vim.keymap.set("n", "<leader>flg", function()
+        require("fzf-lua").live_grep()
+      end, { desc = "FzfLua live grep" })
+      vim.keymap.set("n", "<leader>fb", "<CMD> FzfLua grep_curbuf<CR>", { desc = "FzfLua current buffer ff" })
+    end,
+  },
 }
