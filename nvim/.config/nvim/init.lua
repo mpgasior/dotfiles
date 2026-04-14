36ae332c0a12350897448bf622991a7178e0524a
nimelo <mr.nimelo@gmail.com>
2026-04-14 16:06:54 +0100

    set everforest theme for nvim and yazi

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -59,6 +59,7 @@ vim.pack.add({
   { src = "https://github.com/nvim-tree/nvim-tree.lua" },
   { src = "https://github.com/folke/which-key.nvim" },
   { src = "https://github.com/folke/tokyonight.nvim" },
+  { src = "https://github.com/neanias/everforest-nvim" },
   { src = "https://github.com/mason-org/mason.nvim" },
   { src = "https://github.com/neovim/nvim-lspconfig" },
   { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
@@ -158,4 +159,5 @@ vim.lsp.config("lua_ls", {
   },
 })
 
-vim.cmd([[colorscheme tokyonight]])
+--vim.cmd([[colorscheme tokyonight]])
+vim.cmd([[colorscheme everforest]])
