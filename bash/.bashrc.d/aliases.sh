3a0c2fa1d84003838f2e6759b3d46b1c538abba2
Mateusz Gasior <mr.nimelo@gmail.com>
2026-04-21 21:35:17 +0100

    added xdg-open redirect

--- bash/.bashrc.d/aliases.sh
+++ bash/.bashrc.d/aliases.sh
@@ -6,3 +6,7 @@ alias lg=lazygit
 if ! podman -v &>/dev/null; then
     alias podman='flatpak-spawn --host podman'
 fi
+
+if ! xdg-open -v &>/dev/null; then
+    alias xdg-open='flatpak-spawn --host xdg-open'
+fi
