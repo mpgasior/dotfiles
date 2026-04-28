b6cbbf1cce67ab0af434c8ae62982c4c1b1dcd98
Mateusz Gasior <mr.nimelo@gmail.com>
2026-04-28 21:08:20 +0100

    added status-bar notification when copying text

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 local config = {}
 
@@ -7,6 +8,28 @@ if wezterm.config_builder then
 end
 
 config.window_decorations = "RESIZE"
+config.keys = {
+  {
+    key = 'C',
+    mods = 'CTRL|SHIFT',
+    action = act.Multiple {
+      act.CopyTo 'Clipboard',
+      act.EmitEvent 'show-status-toast',
+    },
+  },
+}
+
+wezterm.on('show-status-toast', function(window, _)
+  -- Set the status to the message
+  window:set_left_status(wezterm.format({
+    { Text = ' 📋 Copied to clipboard! ' },
+  }))
+
+  -- Clear it after 2 seconds
+  wezterm.time.call_after(2.0, function()
+    window:set_left_status('')
+  end)
+end)
 
 config.mouse_bindings = {
   -- This disables the paste action on middle click
